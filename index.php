e3ceff02c368c632c407f652ac47ef38bfe4555b
Jose_Becerra <Jose_Becerra@epam.com>
2022-02-16 19:56:51 -0600

    index.php info.php

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php 
+
+$helloWorld = "Hello World from Google Cloud! 2022";
+
+?>
+
+<html>
+
+    <head>
+
+        <title><?php echo $helloWorld ?></title>
+
+    </head>
+
+    <body>
+
+        <h1><?php echo $helloWorld ?></h1>
+
+    </body>
+
+</html>
