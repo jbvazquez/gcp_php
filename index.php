c33ffdaf3cb7df23975084743f980206cdc29759
Jose de Jesus Becerra Vazquez <josedej.becerrav@gmail.com>
2022-09-12 14:58:16 -0500

    dockerfile

--- index.php
+++ index.php
@@ -15,6 +15,10 @@ $helloWorld = "Hello World from Google Cloud! 2022";
     <body>
 
         <h1><?php echo $helloWorld ?></h1>
+		<p> <?php $name = getenv('NAME', true) ?: 'World';?>
+			<br>		
+			<?phpecho sprintf('Hello %s!', $name);?>
+		</p>
 
     </body>
 
